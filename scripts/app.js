d6cfc341585cb7587bb87656d9f2a44d20e42f0c
Lina Gromakova <linaWebDev@outlook.com>
2024-05-26 19:10:14 +0300

    Add animation

--- scripts/app.js
+++ scripts/app.js
@@ -29,39 +29,39 @@ async function getWeather() {
     ? (date = 'day')
     : (date = 'night');
   generateHTML = `
-        <img src="img/${
-          response.weather[0].main
-        }-${date}.png" alt="weather-icon" />
-        <div class="temperature">
-          <span>${Math.floor(response.main.temp).toFixed(0)}</span>
-          <span class="celsius">℃</span>
+    <div class="wrapper">
+      <img src="img/${
+        response.weather[0].main
+      }-${date}.png" alt="weather-icon" />
+      <div class="temperature">
+        <span>${Math.floor(response.main.temp).toFixed(0)}</span>
+        <span class="celsius">℃</span>
+      </div>
+      <h3>${response.weather[0].main}</h3>
+      <div class="column-wrapper-values">
+      <div class="column-values">
+        <div class="column">
+          <h3 class="symbol">&#8779;</h3>
         </div>
-        <h3>${response.weather[0].main}</h3>
+        <div class="column">
+          <h6>${response.main.humidity}%</h6>
+          <h6>Humidity</h6>
         </div>
-        <div class="column-wrapper-values">
-        <div class="column-values">
-          <div class="column">
-            <h3 class="symbol">&#8779;</h3>
-          </div>
-          <div class="column">
-            <h6>${response.main.humidity}%</h6>
-            <h6>Humidity</h6>
-          </div>
+      </div>
+      <div class="column-values">
+        <div class="column">
+          <h3 class="symbol">༄</h3>
         </div>
-        <div class="column-values">
-          <div class="column">
-            <h3 class="symbol">༄</h3>
-          </div>
-          <div class="column">
-            <h6>${Math.floor(response.wind.speed)}Km/h</h6>
-            <h6>Wind Speed</h6>
-          </div>
+        <div class="column">
+          <h6>${Math.floor(response.wind.speed)}Km/h</h6>
+          <h6>Wind Speed</h6>
         </div>
-      `;
+      </div>
+    </div>
+    `;
   localStorage.setItem('card', JSON.stringify(generateHTML));
   localStorage.setItem('city', inpEl.value);
   return (infoForecastHTML.innerHTML = generateHTML);
 }
-
 infoForecastHTML.innerHTML = JSON.parse(localStorage.getItem('card'));
 inpEl.value = localStorage.getItem('city');
